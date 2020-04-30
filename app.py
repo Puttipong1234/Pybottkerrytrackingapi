36916f735cc84d59aee676e16721aaaf73a21077
Puttipong1234 <Puttipong.lims@gmail.com>
2020-04-30 23:42:18 +0700

    make it better

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def kerry():
     r = Get_data_by_tracking(tracking_number=tracking_number,driver=driver)
     return jsonify(r)
 
-if __name__ == "__main__":
-    app.run(port=8000,debug=True)
+# if __name__ == "__main__":
+#     app.run(port=8000,debug=True)
